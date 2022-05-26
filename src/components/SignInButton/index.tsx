6c2bc1898da2414d1c1a9a94ad55e14789c9d9f6
VitorHRD <vrdev435@gmail.com>
2022-05-26 01:44:47 -0300

    AddStripeAndAuth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,27 +1,24 @@
 import { FaGithub } from 'react-icons/fa'
-import {FiX} from 'react-icons/fi'
+import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
-    return  isUserLoggedIn ? (
-        <button 
-        type="button"
-        className={styles.signInButton}
-        >
-            <FaGithub color="#04d361"/>
-            VitorHRD
-            <FiX color="#737380" className={styles.closeIcon}/>
-        </button>
+  const { data: session } = useSession()
 
-    ) : (
-        <button 
-        type="button"
-        className={styles.signInButton}
-        >
-            <FaGithub color="#eba417"/>
-            Sign In Github
-        </button>
-    );
+  return session ? (
+    <button type="button" className={styles.signInButton}
+      onClick={() => signOut()}>
+      <FaGithub color="#04d361"/>
+      { session.user.name }
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton} 
+      onClick={() => signIn('github')}>
+      <FaGithub color="#eba417"/>
+      Inscreva-se com o GitHub
+    </button>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
 import styles from './home.module.scss'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { GetStaticProps } from 'next'
+import { stripe } from '../services/stripe'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+export default function Home({ product } : HomeProps) {
   return (
     <>
       <Head><title> Home | ig.news</title></Head>
@@ -12,13 +20,29 @@ export default function Home() {
           <h1>Notícias sobre o mundo do <span>React</span></h1>
           <p>
             Para ter acesso a todas publicações <br />
-            <span>Somente R$9,90 Por mês</span>
+            <span>Somente {product.amount} Por mês</span>
           </p>
-          <SubscribeButton/>
+          <SubscribeButton priceId = {product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Garota programando"></img>
       </main>
     </>
   )
+}
+export const getStaticProps : GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1L3RA8AUFBYOfB4EKaDz5ddI')
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('pt-BR', {
+      style:'currency',
+      currency:'BRL'
+    },).format(price.unit_amount / 100)
+  }
+  return {
+    props: {
+      product,
+    },
+    revalidate:60 * 60 * 24, // 24 hours
+  }
 }
