42daef2060ec8263aa6bc8000128c5588e061c1e
VitorHRD <vrdev435@gmail.com>
2022-05-28 01:46:14 -0300

    addFaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { query as q } from "faunadb"
+import { fauna } from '../../../services/fauna'
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -12,5 +14,39 @@ export default NextAuth({
         },
       },
     }),
-  ]
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      const UserEmail = user.email
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(UserEmail)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { UserEmail } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(UserEmail)
+              )
+            )
+          )
+        )
+        return true
+      }
+      catch {
+        return false
+      }
+    }
+  }
+
 })
